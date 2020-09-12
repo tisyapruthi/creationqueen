a52be91a35e4dab351cd156d5a8f1c4427fad282
tisyapruthi <tisya.pruthi@gmail.com>
2020-09-12 13:11:00 -0700

    My calendar program, using string variables.
    My calendar program, using string variables.

---
+++ LMSClub/mycalender1/mycalendar1.py
@@ -0,0 +1,45 @@
+'''Day           Time.                        Subject
+Monday   10:00 - 11:00 AM    Period 1
+                  11:30-12:30 AM.    Math
+Tuesday     9:00-11:00 AM     PE
+                  10:00-11:00 AM.    Science
+'''
+
+day = "Monday"
+time = "9:10 - 10:15"
+subject = "ELA"
+
+print ("Day\t\tTime\t\tSubject ")
+print (day + "\t\t" + time + "\t" + subject)
+
+
+day = "Monday"
+time = "10:35 - 11:40"
+subject = "Social Studies"
+
+print (day + "\t\t" + time + "\t" + subject)
+
+day = "Monday"
+time = "12:10 - 1:15"
+subject = "PE"
+
+print (day + "\t\t" + time + "\t" + subject)
+
+day = "Tuesday"
+time = "9:10 - 10:15"
+subject = "Elective"
+
+print (day + "\t\t" + time + "\t" + subject)
+
+day = "Tuesday"
+time = "10:35 - 11:40"
+subject = "Math"
+
+
+print (day + "\t\t" + time + "\t" + subject)
+
+day = "Tuesday"
+time = "12:10 - 1:15"
+subject = "Science"
+
+print (day + "\t\t" + time + "\t" + subject)
