919a809bf13a251925eb0822c4f83d53cf717e06
tisyapruthi <70452356+tisyapruthi@users.noreply.github.com>
2020-09-19 12:02:29 -0700

    My Calendar #2 (With FUNCTIONS)

---
+++ LMSClub/mycalendar2/mycalendar2.py
@@ -0,0 +1,55 @@
+'''Day           Time.                        Subject
+Monday   10:00 - 11:00 AM    Period 1
+                  11:30-12:30 AM.    Math
+Tuesday     9:00-11:00 AM     PE
+                  10:00-11:00 AM.    Science
+'''
+
+def PrintMyPeriod (day, time, subject):
+	print (day + "\t\t" + time + "\t" + subject)
+
+
+print ("Day\t\tTime\t\tSubject ")
+
+day = "Monday"
+time = "9:10 - 10:15"
+subject = "ELA"
+
+PrintMyPeriod (day, time, subject)
+
+
+day = "Monday"
+time = "10:35 - 11:40"
+subject = "Social Studies"
+
+PrintMyPeriod (day, time, subject)
+
+
+day = "Monday"
+time = "12:10 - 1:15"
+subject = "PE"
+
+PrintMyPeriod (day, time, subject)
+
+
+day = "Tuesday"
+time = "9:10 - 10:15"
+subject = "Elective"
+
+PrintMyPeriod (day, time, subject)
+
+
+day = "Tuesday"
+time = "10:35 - 11:40"
+subject = "Math"
+
+PrintMyPeriod (day, time, subject)
+
+
+
+day = "Tuesday"
+time = "12:10 - 1:15"
+subject = "Science"
+
+PrintMyPeriod (day, time, subject)
+
